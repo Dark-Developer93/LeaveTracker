24ac3d358c9108d5804d9e28c089644ad2f1bb7e
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-12 17:50:45 +0200

    Feat: Update DashLayout component to pass user prop to SideBar and Header components

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,17 +1,20 @@
 import Header from "@/components/Common/Header";
 import SideBar from "@/components/Common/SideBar";
+import { getCurrentUser } from "@/lib/session";
+import { User } from ".prisma/client";
 
 export default async function DashLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getCurrentUser();
   return (
     <section>
       <div className="min-h-screen bg-slate-100 dark:bg-black">
-        <SideBar />
+        <SideBar user={user as User} />
         <div className="sm:ml-[6rem]">
-          <Header />
+          <Header user={user as User} />
           {children}
         </div>
       </div>
