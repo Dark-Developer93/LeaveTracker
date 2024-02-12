301d2723805c59cf1d92ab2571ef159d0ef88436
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-12 18:44:34 +0200

    Feat: Add PortalLayout and WelcomeBanner components

--- app/(portal)/portal/page.tsx
+++ app/(portal)/portal/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { User } from "@prisma/client";
+import WelcomeBanner from "./WelcomeBanner";
+import { getCurrentUser } from "@/lib/session";
 
-const Portal = () => {
-  return <div>Portal</div>;
+const Portal = async () => {
+  const user = await getCurrentUser();
+  return (
+    <>
+      <WelcomeBanner user={user as User} />
+    </>
+  );
 };
 
 export default Portal;
