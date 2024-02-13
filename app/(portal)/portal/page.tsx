fe7f56a201d5fe550a98aa5a22b0ae8c850ab4bb
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-13 19:59:39 +0200

    Feat: Add Calendar component to Portal page

--- app/(portal)/portal/page.tsx
+++ app/(portal)/portal/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { User } from "@prisma/client";
 import WelcomeBanner from "./WelcomeBanner";
 import { getCurrentUser } from "@/lib/session";
+import Calendar from "./Calendar";
 
 const Portal = async () => {
   const user = await getCurrentUser();
   return (
     <>
       <WelcomeBanner user={user as User} />
+      <Calendar />
     </>
   );
 };
