dcacf448f830f0115200a4ae3dfc8cc8a931c11f
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-13 19:59:24 +0200

    Fix: Update logo image in SideBar and SideBarDrawer components

--- components/Common/SideBarDrawer.tsx
+++ components/Common/SideBarDrawer.tsx
@@ -34,7 +34,7 @@ const SideBarDrawer = ({ user }: SideBarDrawerProps) => {
       <SheetContent side="left" className="flex flex-col justify-between w-52">
         <div>
           <div className="flex mt-3 justify-center">
-            <Image src="/spana-b.png" width={50} height={50} alt="logo" />
+            <Image src="/logo.png" width={50} height={50} alt="logo" />
           </div>
           <nav className="flex flex-col items-center px-3 overflow-y-auto">
             {user?.role === "ADMIN" && adminRouter()}
