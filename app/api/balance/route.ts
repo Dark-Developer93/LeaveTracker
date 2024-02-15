1c4423171f40affb95783e9ba117d7b7a482b078
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-15 10:26:00 +0200

    Feat: Add route for submitting credits

--- app/api/balance/route.ts
+++ app/api/balance/route.ts
@@ -2,16 +2,69 @@ import { getCurrentUser } from "@/lib/session";
 import { Role } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+type SubmittedCredits = {
+  annual: number;
+  family: number;
+  sick: number;
+  study: number;
+  maternity: number;
+  paternity: number;
+  unpaid: number;
+  email: string;
+  year: string;
+  name: string;
+};
+
 const allowedRoles = ["ADMIN", "MODERATOR"];
 
 export async function POST(req: NextRequest) {
   const loggedInUser = await getCurrentUser();
   if (!allowedRoles.includes(loggedInUser?.role as Role)) {
-    throw new Error("You are not authorized to perform this action");
+    throw new Error("You are not permitted to perform this action");
   }
-
   try {
-    console.log(req.json);
+    const body: SubmittedCredits = await req.json();
+
+    const {
+      annual,
+      family,
+      sick,
+      study,
+      maternity,
+      paternity,
+      year,
+      email,
+      name,
+    } = body;
+
+    const existingCredits = await prisma.balances.findFirst({
+      where: {
+        year,
+        email,
+      },
+    });
+
+    if (existingCredits) {
+      return NextResponse.json(
+        "Credits for the current period already exists",
+        { status: 400 }
+      );
+    }
+
+    await prisma.balances.create({
+      data: {
+        name,
+        email,
+        year,
+        annualCredit: annual,
+        familyCredit: family,
+        sickCredit: sick,
+        studyCredit: study,
+        maternityCredit: maternity,
+        paternityCredit: paternity,
+      },
+    });
+
     return NextResponse.json({ message: "Success" }, { status: 200 });
   } catch (error) {
     console.error(error);
