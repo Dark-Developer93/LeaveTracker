e5cd483fd44e54c2f9af64f959d5d0ad96363cbc
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-02-15 10:26:33 +0200

    RFR: Update Prisma schema and add troubleshooting link

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,5 @@
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
 import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
