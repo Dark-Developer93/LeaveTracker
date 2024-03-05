a7e7bcaa515f38db784b6c29c46edd837560dde2
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-03-05 09:43:01 +0200

    Feat: Add events icon and route to dashboard

--- components/Common/Routes.tsx
+++ components/Common/Routes.tsx
@@ -1,31 +1,32 @@
 import { HiOutlineUserGroup } from "react-icons/hi";
 import {
   HiMiniComputerDesktop,
-  HiOutlineCog6Tooth,
   HiOutlineSquares2X2,
 } from "react-icons/hi2";
-import { MdOutlineBalance } from "react-icons/md";
+import { MdOutlineBalance, MdEvent, MdHistory  } from "react-icons/md";
 import { TbListCheck } from "react-icons/tb";
 
 export const AdminRoutes = [
   { title: "Portal", url: "/portal", icon: HiMiniComputerDesktop },
   { title: "Dashboard", url: "/dashboard", icon: HiOutlineSquares2X2 },
+  { title: "History", url: "/portal/history", icon: MdHistory },
   { title: "Balances", url: "/dashboard/balances", icon: MdOutlineBalance },
   { title: "Leaves", url: "/dashboard/leaves", icon: TbListCheck },
   { title: "Users", url: "/dashboard/users", icon: HiOutlineUserGroup },
-  { title: "Setting", url: "/dashboard/setting", icon: HiOutlineCog6Tooth },
+  { title: "Events", url: "/dashboard/events", icon: MdEvent },
 ];
 
 export const UserRoutes = [
   { title: "Portal", url: "/portal", icon: HiMiniComputerDesktop },
-  { title: "History", url: "/portal/history", icon: TbListCheck },
+  { title: "History", url: "/portal/history", icon: MdHistory },
 ];
 
 export const ModeratorRoutes = [
   { title: "Portal", url: "/portal", icon: HiMiniComputerDesktop },
   { title: "Dashboard", url: "/dashboard", icon: HiOutlineSquares2X2 },
+  { title: "History", url: "/portal/history", icon: MdHistory },
   { title: "Balances", url: "/dashboard/balances", icon: MdOutlineBalance },
   { title: "Leaves", url: "/dashboard/leaves", icon: TbListCheck },
   { title: "Users", url: "/dashboard/users", icon: HiOutlineUserGroup },
-  { title: "Setting", url: "/dashboard/setting", icon: HiOutlineCog6Tooth },
+  { title: "Events", url: "/dashboard/events", icon: MdEvent },
 ];
