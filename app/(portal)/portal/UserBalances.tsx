6310e252e1e7dd11617a2b0457aaeacdd7f68604
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-03-05 09:57:24 +0200

    RFR: Refactor leave card balance calculation

--- app/(portal)/portal/UserBalances.tsx
+++ app/(portal)/portal/UserBalances.tsx
@@ -15,48 +15,48 @@ const UserBalances = ({ balances }: Props) => {
           leaveType={"ANNUAL"}
           credit={balances?.annualCredit as number}
           used={balances?.annualUsed as number}
-          balance = {balances?.annualCredit as number - (balances?.annualUsed || 0) as number}
-        //   balance={balances?.annualAvailable as number}
+          // balance = {balances?.annualCredit as number - (balances?.annualUsed || 0) as number}
+          balance={balances?.annualAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
           leaveType={"STUDY"}
           credit={balances?.studyCredit as number}
           used={balances?.studyUsed as number}
-          balance = {balances?.studyCredit as number - (balances?.studyUsed || 0) as number}
-        //   balance={balances?.studyAvailable as number}
+          // balance = {balances?.studyCredit as number - (balances?.studyUsed || 0) as number}
+          balance={balances?.studyAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
           leaveType={"SICK"}
           credit={balances?.sickCredit as number}
           used={balances?.sickUsed as number}
-          balance = {balances?.sickCredit as number - (balances?.sickUsed || 0) as number}
-        //   balance={balances?.sickAvailable as number}
+          // balance = {balances?.sickCredit as number - (balances?.sickUsed || 0) as number}
+          balance={balances?.sickAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
           leaveType={"FAMILY"}
           credit={balances?.familyCredit as number}
           used={balances?.familyUsed as number}
-          balance = {balances?.familyCredit as number - (balances?.familyUsed || 0) as number}
-        //   balance={balances?.familyAvailable as number}
+          // balance = {balances?.familyCredit as number - (balances?.familyUsed || 0) as number}
+          balance={balances?.familyAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
           leaveType={"PATERNITY"}
           credit={balances?.paternityCredit as number}
           used={balances?.paternityUsed as number}
-          balance = {balances?.paternityCredit as number - (balances?.paternityUsed || 0) as number}
-        //   balance={balances?.paternityAvailable as number}
+          // balance = {balances?.paternityCredit as number - (balances?.paternityUsed || 0) as number}
+          balance={balances?.paternityAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
           leaveType={"MATERNITY"}
           credit={balances?.maternityCredit as number}
           used={balances?.maternityUsed as number}
-          balance = {balances?.maternityCredit as number - (balances?.maternityUsed || 0) as number}
-        //   balance={balances?.maternityAvailable as number}
+          // balance = {balances?.maternityCredit as number - (balances?.maternityUsed || 0) as number}
+          balance={balances?.maternityAvailable as number}
         />
         <LeaveCard
           year={balances?.year}
