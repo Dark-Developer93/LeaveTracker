37922a347a6a37e704e537251612124f5b128145
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-03-05 13:45:20 +0200

    Feat:" Add close button adn new button variation to DialogWrapper component

--- components/Common/DialogWrapper.tsx
+++ components/Common/DialogWrapper.tsx
@@ -1,8 +1,10 @@
 import { IconType } from "react-icons";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -12,29 +14,50 @@ import { Button } from "../ui/button";
 type DialogProps = {
   children: React.ReactNode;
   btnTitle?: string;
+  linkTitle?: string;
   title?: string;
   description?: string;
-  isBtn: boolean;
+  isBtn?: boolean;
+  isLink?: boolean;
   icon?: IconType;
   open?: boolean;
   setOpen?: () => void;
+  hasCloseBtn?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const DialogWrapper = ({
   children,
   btnTitle = "Close",
+  linkTitle,
   title = "Title",
   description = "Description",
-  isBtn = true,
+  isBtn,
+  isLink,
   icon: Icon,
   open,
   setOpen,
+  hasCloseBtn,
+  onClick,
 }: DialogProps) => {
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (onClick) {
+      event.stopPropagation(); // Stop propagation only if onClick is provided
+      onClick(event);
+    }
+ };
+
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
+      <DialogTrigger asChild onClick={handleClick}>
         {isBtn ? (
           <Button className="text-white">{btnTitle}</Button>
+        ) : isLink ? (
+          <Button variant="link" className="p-0 m-0 h-5 text-secondary-foreground font-normal">
+            {linkTitle}
+          </Button>
         ) : (
           Icon && <Icon className=" text-blue-600 cursor-pointer" size={24} />
         )}
@@ -45,6 +68,15 @@ const DialogWrapper = ({
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
         {children}
+        {hasCloseBtn && (
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        )}
       </DialogContent>
     </Dialog>
   );
