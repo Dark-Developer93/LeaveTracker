d229081c0feff7f1c4a25a3114615ea97df45f3b
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-03-05 09:49:11 +0200

    Feat: Add MonthDateRangePicker and StatsCards components

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Container from "@/components/Common/Container"
+import { MonthDateRangePicker } from "./MonthDateRangePicker"
+import { Button } from "@/components/ui/button"
+import StatsCards from "./StatsCards"
+
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+  return (
+    <Container>
+      <div className="flex flex-col md:flex-row py-6 items-center justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex items-center space-x-2">
+          <MonthDateRangePicker />
+          {/* TODO: add a download method to download the user leave report */}
+          {/* <Button>Download</Button> */}
+        </div>
+      </div>
+      <StatsCards />
+    </Container>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
