b7f615c9f684045b2e84622fe5ffd8b77fdaa6f3
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-23 17:15:11 +0300

    refactor: Update ToggleDarkLight component to use Button component and improve theme toggle functionality

--- components/Common/ToggleDarkLight.tsx
+++ components/Common/ToggleDarkLight.tsx
@@ -1,38 +1,35 @@
 "use client";
 
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { HiOutlineSun } from "react-icons/hi2";
-import { MdOutlineNightlightRound } from "react-icons/md";
+import { Button } from "@/components/ui/button";
 
 const ToggleDarkLight = () => {
   const [mounted, setMounted] = useState(false);
-  const { systemTheme, theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   if (!mounted) {
     return null;
   }
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  return currentTheme === "dark" ? (
-    <button
-      type="button"
-      onClick={() => setTheme("light")}
-      className="p-1 bg-primary text-white rounded-full "
-    >
-      <HiOutlineSun size={24} />
-    </button>
-  ) : (
-    <button
-      type="button"
-      onClick={() => setTheme("dark")}
-      className="p-1 bg-primary text-white rounded-full "
-    >
-      <MdOutlineNightlightRound size={24} />
-    </button>
+
+  return (
+    <Button title="Toggle theme" variant="outline" onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 };
 
