c39122939a7ba30f0c51ce3c66ca13a229eda7d1
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-23 18:06:06 +0300

    refactor: Improve theme toggle functionality in ToggleDarkLight component

--- components/Common/ToggleDarkLight.tsx
+++ components/Common/ToggleDarkLight.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 
 const ToggleDarkLight = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
@@ -20,10 +20,11 @@ const ToggleDarkLight = () => {
   if (!mounted) {
     return null;
   }
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <Button title="Toggle theme" variant="outline" onClick={toggleTheme}>
-      {theme === "dark" ? (
+      {currentTheme === "dark" ? (
         <Sun className="h-[1.2rem] w-[1.2rem]" />
       ) : (
         <Moon className="h-[1.2rem] w-[1.2rem]" />
