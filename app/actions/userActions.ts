3f3af6530f339c015fe341b0a3717a4fc4aa1f20
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-24 15:21:08 +0300

    refactor: Update updateUser function to include supervisors and supervisees in EditUserData

--- app/actions/userActions.ts
+++ app/actions/userActions.ts
@@ -10,22 +10,15 @@ type EditUserData = {
   role: Role;
   title: string;
   id: string;
+  supervisors: string[];
+  supervisees: string[];
 };
 
-export async function updateUser(formData: FormData) {
+export async function updateUser(data: EditUserData) {
   const loggedInUser = await getCurrentUser();
   if (loggedInUser?.role !== "ADMIN") {
     throw new Error("You are not permitted to perform this action");
   }
-
-  const rawData = Object.fromEntries(formData);
-  const data: EditUserData = {
-    id: rawData.id as string,
-    phone: rawData.phone as string,
-    department: rawData.department as string,
-    role: rawData.role as Role,
-    title: rawData.title as string,
-  };
 
   await prisma.user.update({
     where: {
@@ -36,6 +29,8 @@ export async function updateUser(formData: FormData) {
       department: data.department,
       role: data.role,
       title: data.title,
+      supervisors: data.supervisors,
+      supervisees: data.supervisees,
     },
   });
 
